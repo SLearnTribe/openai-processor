3d530a50d19c0cab911e871d67bd0a6af4fe8180
Sai Nandan Pai <sainandanpai@gmail.com>
2023-02-06 16:37:50 +0530

    Updating service

--- src/main/java/com/smilebat/learntribe/openai/services/ChallengeFactory.java
+++ src/main/java/com/smilebat/learntribe/openai/services/ChallengeFactory.java
@@ -4,8 +4,12 @@ import com.google.common.base.Function;
 import com.google.common.base.Verify;
 import com.smilebat.learntribe.dataaccess.jpa.entity.Challenge;
 import com.smilebat.learntribe.learntribeclients.openai.OpenAiService;
+import com.smilebat.learntribe.openai.OpenAiRequest;
+import com.smilebat.learntribe.openai.response.Choice;
+import com.smilebat.learntribe.openai.response.OpenAiResponse;
 import com.smilebat.learntribe.openai.services.helpers.ChallengeParser;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import lombok.Builder;
 import lombok.Getter;
@@ -65,37 +69,42 @@ public class ChallengeFactory {
 
   private Set<Challenge> getOpenAiCompletions(String prompt) {
     log.info("Prompt: {}", prompt);
-    //    OpenAiRequest request = new OpenAiRequest();
-    //    request.setPrompt(prompt);
-    //    final OpenAiResponse completions = openAiService.getCompletions(request);
-    //    final List<Choice> choices = completions.getChoices();
-    //    if (choices == null || choices.isEmpty()) {
-    //      log.info("Unable to create open ai completion text");
-    //      throw new IllegalArgumentException();
-    //    }
-    //    Choice choice = choices.get(0);
-    // String completedText = choice.getText();
+    OpenAiRequest request = new OpenAiRequest();
+    request.setPrompt(prompt);
+    final OpenAiResponse completions = openAiService.getCompletions(request);
+    final List<Choice> choices = completions.getChoices();
+    if (choices == null || choices.isEmpty()) {
+      log.info("Unable to create open ai completion text");
+      throw new IllegalArgumentException();
+    }
+    Choice choice = choices.get(0);
+    String completedText = choice.getText();
 
-    String completedText =
-        "\n\n1. What is the name of the software development environment"
-            + "in"
-            + "which Java is primarily used?\n\nA. Eclipse\nB. NetBeans\nC. IntelliJ IDEA\nD. Android"
-            + "Studio\n\nAnswer: C. IntelliJ IDEA\n\n2. What is the file extension for Java source"
-            + "files?\n\nA. .java\nB. .class\nC. .jar\nD. .exe\n\nAnswer: A. .java\n\n3. Which of the"
-            + "following is not a Java primitive type?\n\nA. int\nB. float\nC. String\nD."
-            + "char\n\nAnswer: C."
-            + "String\n\n4. Which of the following is not a keyword in Java?\n\nA. public\nB."
-            + "static\nC."
-            + "void\nD. native\n\nAnswer: D. native\n\n5. What is the output of the following"
-            + "code?\n\npublic class Test {\n   public static void main(String[] args) {\n"
-            + "System.out.println(\"Hello, world!\");\n   }\n}\n\nA. Hello, world!\nB. 0\nC. Hello,"
-            + "world\nD. compilation error\n\nAnswer: A. Hello, world!"
-            + "\n\n6. Which of the following is not in Java?\n\nA. let\nB."
-            + "static\nC."
-            + "void\nD. mongo\n\nAnswer: D. mongo\n\n7. What is the output of the following"
-            + "code?\n\npublic class Test {\n   public static void main(String[] args) {\n"
-            + "System.out.println(\"Hello, world!\");\n   }\n}\n\nA. Hello, world!\nB. 0\nC. Helloooo,"
-            + "world\nD. compilation err\n\nAnswer: A. Hell0o, world!";
+    //    String completedText =
+    //        "\n\n1. What is the name of the software development environment"
+    //            + "in"
+    //            + "which Java is primarily used?\n\nA. Eclipse\nB. NetBeans\nC. IntelliJ IDEA\nD.
+    // Android"
+    //            + "Studio\n\nAnswer: C. IntelliJ IDEA\n\n2. What is the file extension for Java
+    // source"
+    //            + "files?\n\nA. .java\nB. .class\nC. .jar\nD. .exe\n\nAnswer: A. .java\n\n3. Which
+    // of the"
+    //            + "following is not a Java primitive type?\n\nA. int\nB. float\nC. String\nD."
+    //            + "char\n\nAnswer: C."
+    //            + "String\n\n4. Which of the following is not a keyword in Java?\n\nA. public\nB."
+    //            + "static\nC."
+    //            + "void\nD. native\n\nAnswer: D. native\n\n5. What is the output of the following"
+    //            + "code?\n\npublic class Test {\n   public static void main(String[] args) {\n"
+    //            + "System.out.println(\"Hello, world!\");\n   }\n}\n\nA. Hello, world!\nB. 0\nC.
+    // Hello,"
+    //            + "world\nD. compilation error\n\nAnswer: A. Hello, world!"
+    //            + "\n\n6. Which of the following is not in Java?\n\nA. let\nB."
+    //            + "static\nC."
+    //            + "void\nD. mongo\n\nAnswer: D. mongo\n\n7. What is the output of the following"
+    //            + "code?\n\npublic class Test {\n   public static void main(String[] args) {\n"
+    //            + "System.out.println(\"Hello, world!\");\n   }\n}\n\nA. Hello, world!\nB. 0\nC.
+    // Helloooo,"
+    //            + "world\nD. compilation err\n\nAnswer: A. Hell0o, world!";
     return challengeParser.parseText(completedText);
   }
 }
